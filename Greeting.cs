d16a5bf84e6f473c72bc5eb3628bb8b373fe98a0
thanou <thanou.mckinsey@gmail.com>
2020-04-08 15:18:15 -0400

    added a simple test to a RESTful call

---
+++ Greeting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace api_testing
+{
+    public class Greeting
+    {
+        public string name { get; set; }
+        public string message { get; set; }
+        public Greeting()
+        {
+        }
+    }
+}
---
+++ GreetingTest.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace api_testing
+{
+    public class GreetingTest
+    {
+        string URL = "https://zoomworld.azurewebsites.net/api/zoomworld20?code=ake7hRhu8e4hGNKzIQmJ45q6PA5E3kL520QKRD4PZamTQRYhktYK9w==&name=Bob";
+        HttpClient apiClient = new HttpClient();
+        [Fact]
+        public async Task TestGreetingApi()
+        {
+            try
+            {
+                HttpResponseMessage response = await apiClient.GetAsync(URL);
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Greeting greeting = JsonSerializer.Deserialize<Greeting>(responseBody);
+                Assert.Equal("Bob", greeting.name);
+                Assert.Equal("Hello there", greeting.message);
+            }
+            catch (HttpRequestException ex)
+            {
+                Assert.True(false, ex.Message);
+            }
+
+        }
+    }
+}
